c8c128e4fc3e791cd6665355d417029608ec5754
Kumar Deepanshu <kumardeepanshu157@gmail.com>
2020-12-19 16:20:48 +0530

    Meme  done

--- client/src/Components/Memes.js
+++ client/src/Components/Memes.js
@@ -4,37 +4,33 @@ import MemeImage from "../images/Meme.jpeg";
 // import MemeContainer from "./Containers/MemeContainer";
 import "./meme.css"
 const Memes = () => {
-  // const [memes, setMemes] = useState([]);
-  // const url =
-  //   "https://raw.githubusercontent.com/alenwelch/image-/main/api/Data.json";
-  // useEffect(() => {
-  //   async function getData() {
-  //     const request = await axios.get(url);
-  //     setMemes(request.data);
-  //     console.log(request.data);
-  //     return request;
-  //   }
-  //   getData();
-  // }, [url]);
+  const [memes, setMemes] = useState([]);
+  const url =
+    "https://raw.githubusercontent.com/alenwelch/image-/main/api/Data.json";
+  useEffect(() => {
+    async function getData() {
+      const request = await axios.get(url);
+      setMemes(request.data);
+      console.log(request.data);
+      return request;
+    }
+    getData();
+  }, [url]);
   return (
     <div className="meme">
-      {/* {memes.map((meme) => {
+      <div className="main-meme">      <div className="meme-container">
+        {memes.map((image) => (
+          <div className="card-content">         
+            <img src={image.img} alt="avatar" className="image"/>
+            
+          </div>
+        ))} 
        
-         <MemeContainer image={memes}
         
-      })} */}
-      {/* {memes.map((meme) => return (
-        <MemeContainer image={memes.img} />;
-      ))} */}
-      <div className="meme-container">
-        {/* {memes.map((image) => (
-          <MemeContainer image={image.img} />
-        ))} */}
-        <div className="card">
-            <img src={MemeImage} alt="avatar" className="image"/>
-        </div>
+        </div></div>
+
       </div>
-    </div>
+    
   );
 };
 
